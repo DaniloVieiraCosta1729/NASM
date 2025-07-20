2cd1ba11c557da1cd1231c405227175b4f551e22
Danilo <danilo.11235@outlook.com>
2025-07-20 00:37:45 -0300

    escrevendo um ola, mundo com a assembly nasm. Bem bacam.

---
+++ Ola.asm
@@ -0,0 +1,15 @@
+section .data
+	mensagem db 'Ola, mundo!'
+
+section .text
+	global _start
+
+_start:
+	mov rax, 1; identificador da sys_call que escreve
+	mov rdi, 1; significa saida padrao
+	mov rsi, mensagem; ponteiro para o lugar da memoria que guarda o conteudo de mensagem
+	mov rdx, 13; o registrador rdx guarda aqui o tamanho da variavel
+	syscall
+	mov rax, 60; system call que encerra o programa
+	mov rdi, 0; primeiro parametro da syscall 60. O 0 indica que nao houve erros
+	syscall
