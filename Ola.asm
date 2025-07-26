31e951b070220ec66c43a68e30c3107a7f3f8851
Danilo <danilo.11235@outlook.com>
2025-07-26 19:19:18 -0300

    pulando uma linha e entendendo o uso do ASCII em assembly

--- Ola.asm
+++ Ola.asm
@@ -1,5 +1,5 @@
 section .data
-	mensagem db 'Ola, mundo! Finalmente estou entendendo o Assembly =)'
+	mensagem db 'Ola, mundo! Finalmente estou entendendo o Assembly =)', 10
 
 section .text
 	global _start
@@ -8,7 +8,7 @@ _start:
 	mov rax, 1; identificador da sys_call que escreve
 	mov rdi, 1; significa saida padrao
 	mov rsi, mensagem; ponteiro para o lugar da memoria que guarda o conteudo de mensagem
-	mov rdx, 90; o registrador rdx guarda aqui o tamanho da variavel
+	mov rdx, 54; o registrador rdx guarda aqui o tamanho da variavel
 	syscall
 	mov rax, 60; system call que encerra o programa
 	mov rdi, 0; primeiro parametro da syscall 60. O 0 indica que nao houve erros
