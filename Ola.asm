da99bc5479a55509f9b60652c7a97c086624ac0e
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-07-26 19:52:14 -0300

    Aprendendo a usar constantes na section .data e trocando valores escritos de forma explicita por essas constantes.

--- Ola.asm
+++ Ola.asm
@@ -1,5 +1,9 @@
 section .data
 	mensagem db 'Ola, mundo! Finalmente estou entendendo o Assembly =)', 10
+	jeitoEstranho db 0x4F,108,97,44,32,0x4d,0x75,0x6e,0x64,111,10
+
+	tamnhoJeitoEstranho equ 11
+	tamanhoMensagem equ 54
 
 section .text
 	global _start
@@ -7,9 +11,16 @@ section .text
 _start:
 	mov rax, 1; identificador da sys_call que escreve
 	mov rdi, 1; significa saida padrao
-	mov rsi, mensagem; ponteiro para o lugar da memoria que guarda o conteudo de mensagem
-	mov rdx, 54; o registrador rdx guarda aqui o tamanho da variavel
+	mov rsi, jeitoEstranho; ponteiro para o lugar da memoria que guarda o conteudo de mensagem
+	mov rdx, 11; o registrador rdx guarda aqui o tamanho da variavel
 	syscall
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, mensagem
+	mov rdx, tamanhoMensagem
+	syscall
+
 	mov rax, 60; system call que encerra o programa
 	mov rdi, 0; primeiro parametro da syscall 60. O 0 indica que nao houve erros
 	syscall
