e684085ad881657f9a7e98a742d475da7ed74be2
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-07-27 12:14:48 -0300

    Coloca labels no código para deixar ele mais modular e para facilitar debug

--- Ola.asm
+++ Ola.asm
@@ -11,24 +11,38 @@ section .text
 	global _start
 
 _start:
+
+;Usando labels para poder debuggar o codigo
+	call mostraMsgA
+	call mostraMsgB
+	call mostraMsgC
+	call encerraPrograma
+
+mostraMsgA:
 	mov rax, 1; identificador da sys_call que escreve
 	mov rdi, 1; significa saida padrao
 	mov rsi, jeitoEstranho; ponteiro para o lugar da memoria que guarda o conteudo de mensagem
 	mov rdx, 11; o registrador rdx guarda aqui o tamanho da variavel
 	syscall
+	ret
 	
+mostraMsgB:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, mensagem
 	mov rdx, tamanhoMensagem
 	syscall
+	ret
 
+mostraMsgC:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, novaMensagem
 	mov rdx, tamNovaMensagem
 	syscall
+	ret
 
+encerraPrograma:
 	mov rax, 60; system call que encerra o programa
 	mov rdi, 0; primeiro parametro da syscall 60. O 0 indica que nao houve erros
 	syscall
