fb88a7d09236cee224f07f5e556c02fc8bff2aec
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-09-07 15:15:43 -0300

    Implementa log_2(x) como função..

---
+++ ex_guanabara_assembly/preparacao_ex06/myLog.asm
@@ -0,0 +1,21 @@
+; ======================= TÍTULO =======================
+; Aproximação do logaritmo na base 2
+
+section .data
+    extrai dq 0b011111111111
+
+    lg_e dq 1.442695040888963407
+
+section .text
+    global lg
+    global ln
+
+; Recebe um float64 (double precision) em rdi
+; Retorna lg([rdi]) em rax
+; OBS: o valor em rdi já deve estar de acordo com IEEE-754 standard para double-precision floating-point.
+lg:
+    shr rdi, 52
+    sub rdi, 1023
+    and rdi, [extrai]
+    mov rax, rdi
+    ret
