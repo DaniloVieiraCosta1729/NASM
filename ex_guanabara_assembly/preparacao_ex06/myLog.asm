878b7fef430edbe2b17cc1ed0e38ba15e185491a
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-09-13 21:17:59 -0300

    Adiciona uma muleta que corrige um problema que eu causei por não ter usado a convenção System V ABI. Correção: adiciona variável que recebe o valor de xmm0 e com isso, passamos o valor para rdi. Isso corrige o fluxo de outras funções que chamam essa, mas retornam em xmm0 e não em rdi.

--- ex_guanabara_assembly/preparacao_ex06/myLog.asm
+++ ex_guanabara_assembly/preparacao_ex06/myLog.asm
@@ -8,14 +8,20 @@ section .data
 
     ln_2 dq 0.693147180559945309
 
+section .bss
+
+    muleta resq 1
+
 section .text
     global lg
     global ln
 
-; Recebe um float64 (double precision) em rdi
+; Recebe um float64 (double precision) em rdi (em xmm0, na verdade, pra não causar erro)
 ; Retorna lg([rdi]) em xmm0
 ; OBS: o valor em rdi já deve estar de acordo com IEEE-754 standard para double-precision floating-point.
 lg:
+    movsd [muleta], xmm0
+    mov rdi, [muleta]
     mov rax, rdi
     shr rax, 52
     and rax, [extrai]
